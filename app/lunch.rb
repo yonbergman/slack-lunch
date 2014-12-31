15894c6774dbfa1e7c2c5a5aa5d5b3da81a9a404
yon <Lighthawky@gmail.com>
2014-12-31 17:25:39 +0200

    fixed redis for heroku

--- app/lunch.rb
+++ app/lunch.rb
@@ -8,7 +8,15 @@ class LunchManager
   attr_reader :key
 
   def initialize()
-    @redis = Redis.new(port: 16379)
+    if ENV["REDISCLOUD_URL"]
+      uri = URI.parse(ENV["REDISCLOUD_URL"])
+      @redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    elsif ENV["BOXEN_REDIS_URL"]
+      uri = URI.parse(ENV["BOXEN_REDIS_URL"])
+      @redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    else
+      @redis = Redis.new
+    end
   end
 
   def add(user, restaurant)
